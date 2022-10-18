cb99ee71401555c2e0f2062a003f581f5cec1fec
Allison Li <y.allisonli@berkeley.edu>
2022-10-18 10:54:15 -0700

    generate RSA keys

--- User.cpp
+++ User.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "shamir.h"
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
 using namespace std;
 
 // Right now only supports storing one key share at a time
@@ -9,6 +13,12 @@ class User {
             cout << "Initializing user " << name << endl;
 
             // Setup public private key pair
+            pRSA = RSA_generate_key(2048, 3, NULL, NULL);
+            
+            BIGNUM *n = BN_new();
+            BIGNUM *e = BN_new();
+            BIGNUM *d = BN_new();
+            RSA_get0_key(pRSA, &n, &e, &d);
         }
 
         int getPublicKey() {
@@ -27,8 +37,8 @@ class User {
     
 
     private:
-        int publicKey;
-        int privateKey;
+        RSA *pRSA;
+
         ShamirShare *keyShare;
 
 };
